09b2b0c8018715a71a2063ceb37664d5b89aa331
s3xmaxman <badside3919@gmail.com>
2024-08-15 09:24:10 +0900

    build(deps): update .gitignore, next.config.mjs, package-lock.json and package.json

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://eb60b03e2e3118ff0c8ebf3c3ee5063b@o4506958884569088.ingest.us.sentry.io/4507778442985472",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
